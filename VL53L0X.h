0c9768fd62fb4cbc8b5c5ac1f5f26b351d139078
Takashi Ando <takashi7ando@gmail.com>
2017-12-15 07:27:28 +0900

    Add constructor for sensor with already changed slave device address
    On using multiple VL53L0X devices as following, the first device's slave
    address was changed to new one 0x54 if I called setAddress() for the
    second device's instance. This commit fixes this issue.
    
    1. 0x52 (default)
    2. 0x54

--- VL53L0X.h
+++ VL53L0X.h
@@ -97,6 +97,7 @@ class VL53L0X
     uint8_t last_status; // status of last I2C transmission
 
     VL53L0X(void);
+    VL53L0X(const uint8_t new_addr);
 
     void setAddress(uint8_t new_addr);
     inline uint8_t getAddress(void) { return address; }
