363babe57fd3cf9afbe57b5ef772933ed32e6402
Pascal Pieper <Accounts@PascalPieper.de>
2020-10-24 11:27:58 +0200

    Rounding up like the original code

--- VL53L0X.cpp
+++ VL53L0X.cpp
@@ -30,7 +30,7 @@
 // Calculate macro period in *nanoseconds* from VCSEL period in PCLKs
 // based on VL53L0X_calc_macro_period_ps()
 // PLL_period_ps = 1655; macro_period_vclks = 2304
-#define calcMacroPeriod(vcsel_period_pclks) ((((uint32_t)2304 * (vcsel_period_pclks) * 1655) + 500) / 100)
+#define calcMacroPeriod(vcsel_period_pclks) ((((uint32_t)2304 * (vcsel_period_pclks) * 1655) + 50) / 100)
 
 // Constructors ////////////////////////////////////////////////////////////////
 
